9c6e443c193fecdc11b460f6e7c78a757e7ccbd1
Nick Damiano <damiano.nick@gmail.com>
2015-06-21 22:31:18 -0500

    Initial commit - includes deck model and deck controller. Deck controller has main methods in it that lay out, in comments, what should happen (mostly with javascript)

---
+++ app/controllers/deck_controller.rb
@@ -0,0 +1,34 @@
+class DeckController < ApplicationController
+
+	def create
+
+	end
+
+	# when create is pressed - post request - ajax save?
+	def save
+		# grab params - parse them - save them into database as json / hash
+		# redirect to study or back to create
+
+	end
+
+	def study
+		# javascript looks in sessionStorage for cards and pulls them into the main deck, pops 5 into active deck and sets up triggerEvents
+		# When active and main are both empty, triggers redirect to review page
+	end
+
+	def review
+		# javascript finds review sessionstorage and loads it into javascript array pile - flip through cards and if you don't know it - 
+		#    save it to to be reviewed. pops it out into the to be reviewed deck(same name study parses). Once main deck is empty, trigger
+		#    redirect to study
+	end
+
+	def select
+		# pull json from database and merge it into one
+		# object 
+		# set instance variable to json - it will be pulled into hidden field
+		# javascript grabs it and saves it in session storage
+		# redirect to study
+	end
+
+
+end
